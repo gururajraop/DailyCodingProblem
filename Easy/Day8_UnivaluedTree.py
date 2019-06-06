0376f8b5c123428dc8e7092e176a52657c04f61c
GURURAJA P RAO <gururajraop@gmail.com>
2019-06-06 18:04:03 +0200

    Add solution to Univalued Tree problem

--- Easy/Day8_UnivaluedTree.py
+++ Easy/Day8_UnivaluedTree.py
@@ -36,14 +36,54 @@ def print_tree(root, space=0):
             print_tree(root.right, space+1)
 
 
-def count_univalued_subtree(Tree):
-    pass
+def check_univalued_tree(root, value):
+    if root.value != value:
+        return False
+
+    if root.left is not None:
+        univalued_tree = check_univalued_tree(root.left, value)
+        if not univalued_tree:
+            return False
+    if root.right is not None:
+        univalued_tree = check_univalued_tree(root.right, value)
+        if not univalued_tree:
+            return False
+
+    return True
+
+
+def count_univalued_subtree(root):
+    if root is None:
+        return 0
+
+    # If leaf node return 1
+    if (root.left is None) and (root.right is None):
+        return 1
+
+    count = 0
+    same_children = True
+    if root.left is not None:
+        if not check_univalued_tree(root.left, root.value):
+            same_children = False
+        count += count_univalued_subtree(root.left)
+
+    if root.right is not None:
+        if not check_univalued_tree(root.right, root.value):
+            same_children = False
+        count += count_univalued_subtree(root.right)
+
+    if same_children:
+        count += 1
+
+    return count
 
 
 if __name__ == '__main__':
     Tree = Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0)))
+    #Tree = Node(1, Node(1, Node(1), Node(1)), Node(1, Node(1), Node(1)))
     str = "Space:" + '' * 0 + ":"
     print("Given input tree is:")
     print_tree(Tree)
+    print("\n\n")
     result = count_univalued_subtree(Tree)
     print("\n\nNumber of univalued subtrees: ", result)
