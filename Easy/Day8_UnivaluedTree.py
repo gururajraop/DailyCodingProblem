b148d808766a6f954ca589fc8446f723fe284020
GURURAJA P RAO <gururajraop@gmail.com>
2019-06-06 16:59:00 +0200

    Add code for printing the binary tree

--- Easy/Day8_UnivaluedTree.py
+++ Easy/Day8_UnivaluedTree.py
@@ -17,4 +17,33 @@ import sys
 import numpy as np
 
 
+class Node(object):
+    def __init__(self, value, left=None, right=None):
+        self.value = value
+        self.left = left
+        self.right = right
+
+
+def print_tree(root, space=0):
+    if root is not None:
+        print(root.value, end = '')
+        if root.left is not None:
+            print("__", end = '')
+            print_tree(root.left, space+1)
+        if root.right is not None:
+            extra = "\n" + '   ' * space + "|__"
+            print(extra, end='')
+            print_tree(root.right, space+1)
+
+
+def count_univalued_subtree(Tree):
+    pass
+
+
 if __name__ == '__main__':
+    Tree = Node(0, Node(1), Node(0, Node(1, Node(1), Node(1)), Node(0)))
+    str = "Space:" + '' * 0 + ":"
+    print("Given input tree is:")
+    print_tree(Tree)
+    result = count_univalued_subtree(Tree)
+    print("\n\nNumber of univalued subtrees: ", result)
