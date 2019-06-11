c693db50d6e5fb014d730416512352b1f28f036d
GURURAJA P RAO <gururajraop@gmail.com>
2019-06-11 12:23:09 +0200

    Remove recursive logic and use loops

--- Hard/Day12_Staircase.py
+++ Hard/Day12_Staircase.py
@@ -18,20 +18,48 @@ import sys
 import numpy as np
 
 
-def get_unique_ways(n, steps):
-    assert (n > 1) and (steps > 0).all() and (steps <= n).all(), "Invalid input"
+def get_unique_ways1(n):
+    assert n > 0, "Invalid input! non-positive input"
 
-    return [1,2]
+    if n == 1:
+        return [[1]]
+    elif n == 2:
+        return [[1, 1], [2]]
+    else:
+        a, b = [[1]], [[1, 1], [2]]
+        for _ in range(n-2):
+            a = [l + [2] for l in a]
+            b = [l + [1] for l in b]
+            a, b = b, a + b
+
+        return b
+
+
+def get_unique_ways2(n, steps):
+    steps = np.array(steps)
+    assert (n > 0) and (steps > 0).all(), "Invalid input! non-positive input"
+
+    if (steps > n).all():
+        print("Steps to climb shouldn't be bigger than total number of steps")
+        return []
+
+    return []
 
 
 if __name__ == '__main__':
-    assert get_unique_ways(4, [1, 2]) == 5, 'Fail: Test 1 failed'
-    assert get_unique_ways(5, [1, 3]) == 4, 'Fail: Test 2 failed'
+    assert len(get_unique_ways1(1)) == 1, 'Fail: Test 1 failed'
+    assert len(get_unique_ways1(2)) == 2, 'Fail: Test 2 failed'
+    assert len(get_unique_ways1(3)) == 3, 'Fail: Test 3 failed'
+    assert len(get_unique_ways1(4)) == 5, 'Fail: Test 4 failed'
+    assert len(get_unique_ways1(5)) == 8, 'Fail: Test 5 failed'
+    #assert len(get_unique_ways2(5, [1, 2])) == 8, 'Fail: Test 6 failed'
+    #assert len(get_unique_ways2(5, [1, 3])) == 4, 'Fail: Test 7 failed'
 
     n = int(input("Enter the number of steps\n"))
-    steps = input("Enter a list of steps that can be climbed, separated by space\n").split()
-    steps = np.asarray(steps, dtype=np.int)
+    steps_in = input("Enter a list of steps that can be climbed, separated by space\n").split()
+    steps = np.sort(np.asarray(steps_in, dtype=np.int))
+    steps = np.array([s for s in steps if s <= n])
 
-    result = get_unique_ways(n, steps)
-    print("Number of ways to climb {} steps being able to climb {} steps at a time is: {}".format(n, steps, len(result)))
+    result = get_unique_ways2(n, steps)
+    print("Number of ways to climb {} steps being able to climb {} steps at a time is: {}".format(n, steps_in, len(result)))
     print("The unique ways are: ", result)
