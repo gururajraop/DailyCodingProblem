2169b8035caf5d6b3ea4515d4a3f484ee698e105
GURURAJA P RAO <gururajraop@gmail.com>
2019-06-12 10:32:10 +0200

    Add solution to largest substring with k distinct chars

--- Hard/Day13_Substring_Kletters.py
+++ Hard/Day13_Substring_Kletters.py
@@ -10,7 +10,38 @@ import numpy as np
 
 
 def find_longest_substring(s, k=2):
-    pass
+    if k == 0:
+        return ""
+    elif k == 1:
+        return s[0]
+    else:
+        substring = ""              # Current substring with max k distinct chars
+        longest_substr = ""         # The longest substring so far with max k distinct chars
+        chars = []                  # Distinct chars in the substring
+        for i, c in enumerate(s):
+            if c in chars:          # The character is already seen in the substring
+                substring = substring + c
+            else:                   # New character to the substring
+                if len(chars) >= k: # The substring already had k distinct characters
+                    # Remove the character that occur farthest to the current character
+                    last_char = substring[-1]
+                    remove_char = chars[0] if last_char == chars[1] else chars[1]
+                    # Obtain the last position of that character
+                    positions = [pos for pos, char in enumerate(substring) if char == remove_char]
+                    last_occ = max(positions)
+                    # Remove the character
+                    substring = substring[last_occ+1:]
+                    chars.remove(remove_char)
+                # Add the character to the substring
+                substring = substring + c
+                chars.append(c)
+            # Replace the longest substring with the substring if substring is longer
+            if len(substring) > len(longest_substr):
+                longest_substr = substring
+
+        print("Given string: {}\t\tk: {}\t longest substring: {}".format(s, k, longest_substr))
+        return longest_substr
+
 
 
 if __name__ == '__main__':
