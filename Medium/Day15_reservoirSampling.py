1ea62c37e0286c9a52740d9c83a600765aa0ca94
GURURAJA P RAO <gururajraop@gmail.com>
2019-06-27 10:07:55 +0200

    Solution to reservoir sampling

--- Medium/Day15_reservoirSampling.py
+++ Medium/Day15_reservoirSampling.py
@@ -8,9 +8,18 @@ import numpy as np
 import random
 
 
-def reservoirSampling():
-    pass
+def reservoirSampling(arr):
+    items = []
+
+    for i, e in enumerate(arr):
+        if random.randint(1, i + 1) == 1:
+            items.append(e)
+
+    return items
 
 
 if __name__ == '__main__':
-    reservoirSampling()
+    n = int(input("Enter the size of the array: "))
+    arr = np.random.randint(low=0, high=100, size=n)
+    item = reservoirSampling(arr)
+    print("Randomly sampled items from the array are: ", item)
