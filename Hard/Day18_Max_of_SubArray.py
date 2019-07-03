bbc1fefa98711002e5de4e6a79c65b35834e2179
GURURAJA P RAO <gururajraop@gmail.com>
2019-07-03 10:21:15 +0200

    Day 18: Add the code for elements with indices less than k

--- Hard/Day18_Max_of_SubArray.py
+++ Hard/Day18_Max_of_SubArray.py
@@ -17,6 +17,33 @@ import sys
 import numpy as np
 
 
+def max_subarray(arr, k):
+    assert (0 < k) and (k <= len(arr)), "Invalid value of k"
+
+    max_item = -np.inf
+    max_index = 0
+    indices = []
+    for i, element in enumerate(arr):
+        if i < k:
+            if max_item < element:
+                max_item = element
+                max_index += 1
+            indices.append(i)
+            if i == k-1:
+                print("Max(", arr[i-k+1:i+1], ") = ", max_item)
+        else:
+            print("Max(", arr[i-k+1:i+1], ") = ", max_item)
+
+
+
+
 
 if __name__ == '__main__':
-    print("Max item of Subarray")
+    # Given test case
+    arr = [10, 5, 2, 7, 8, 7]
+    max_subarray(arr, k=3)
+
+    # Random case
+    #arr = np.random.randint(low=0, high=10, size=10)
+    #print("Input array is: ", arr)
+    #max_subarray(arr, k=3)
