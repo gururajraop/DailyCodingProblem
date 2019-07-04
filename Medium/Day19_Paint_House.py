98eec6502d9a03c493d5fd37df927d8d20de60d1
GURURAJA P RAO <gururajraop@gmail.com>
2019-07-04 13:49:03 +0200

    Day 19: working solution

--- Medium/Day19_Paint_House.py
+++ Medium/Day19_Paint_House.py
@@ -12,10 +12,27 @@ import numpy as np
 
 
 def find_min_cost(cost_matrix):
-    return 0
+    N, K = cost_matrix.shape
+    solution = list(np.zeros((1, K)))
+
+    for h, house in enumerate(cost_matrix):
+        house_cost = []
+        for c, cost in enumerate(house):
+            min_cost = min([solution[h][i] for i in range(K) if i != c])
+            house_cost.append(min_cost + cost)
+        solution.append(house_cost)
+
+    min_cost = min(solution[-1])
+
+    return min_cost
 
 
 if __name__ == '__main__':
+    cost_matrix = np.array([[11, 47], [18, 37], [24, 40]])
+    min_cost = find_min_cost(cost_matrix)
+    print("Total cost of coloring {} houses with {} colors is {}".format(3, 2, min_cost))
+
+
     N = np.random.randint(low=5, high=10, size=1)[0]
     K = np.random.randint(low=2, high=N, size=1)[0]
     cost_matrix = np.random.randint(low=30, high=100, size=(N, K))
