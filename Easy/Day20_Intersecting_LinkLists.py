5167c76e856330abcf9abd5e98df43a80157b6ca
GURURAJA P RAO <gururajraop@gmail.com>
2019-07-04 15:21:48 +0200

    Day 20: Linked list creation

--- Easy/Day20_Intersecting_LinkLists.py
+++ Easy/Day20_Intersecting_LinkLists.py
@@ -11,5 +11,51 @@ import os
 import sys
 import numpy as np
 
+
+class node:
+    def __init__(self, val=0, next=None):
+        self.value = val
+        self.next = next
+
+
+class Linked_List:
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_item(self, item):
+        if self.head is None:
+            self.head = node(val=item)
+            self.tail = self.head
+        else:
+            Node = node(val=item)
+            self.tail.next = Node
+            self.tail = self.tail.next
+
+    def print_list(self):
+        items = []
+        current = self.head
+        while current is not None:
+            items.append(current.value)
+            current = current.next
+
+        print(items)
+
+
+def create_linked_list(lst):
+    root = Linked_List()
+    for item in lst:
+        root.add_item(item)
+
+    return root
+
+
 if __name__ == '__main__':
-    print("Done")
+    list1 = np.random.randint(low=1, high=50, size=10)
+    list1 = create_linked_list(list1)
+    print("Items in the list-1 are: ")
+    list1.print_list()
+    list2 = np.random.randint(low=1, high=50, size=10)
+    list2 = create_linked_list(list2)
+    print("Items in the list-2 are: ")
+    list2.print_list()
